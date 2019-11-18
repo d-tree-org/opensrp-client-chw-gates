52bf748c4774898c351cd8f77a8810093465129f
Isaiah <issyzac.iz@gmail.com>
2019-11-18 13:33:47 +0300

    iM | chore: Conform the flavours to the new updates

--- opensrp-chw/src/at/java/org/smartregister/chw/activity/ChildMedicalHistoryActivityFlv.java
+++ opensrp-chw/src/at/java/org/smartregister/chw/activity/ChildMedicalHistoryActivityFlv.java
@@ -3,6 +3,7 @@ package org.smartregister.chw.activity;
 import android.app.Activity;
 import android.content.Context;
 
+import org.smartregister.chw.core.activity.DefaultChildMedicalHistoryActivityFlv;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
 import java.util.Date;
@@ -11,20 +12,6 @@ import java.util.Map;
 /**
  * Author : Isaya Mollel on 2019-11-12.
  */
-public class ChildMedicalHistoryActivityFlv extends DefaultChildMedicalHistoryActivity implements ChildMedicalHistoryActivity.Flavor {
+public class ChildMedicalHistoryActivityFlv extends DefaultChildMedicalHistoryActivityFlv {
 
-    @Override
-    public void renderView(Activity activity) {
-        super.onViewUpdated(activity);
-    }
-
-    @Override
-    public void fetchData(Context context, Map<String, Date> vaccineList, String dateOfBirth, CommonPersonObjectClient childClient) {
-        //generateHomeVisitServiceList(childClient);
-        setInitialVaccineList(vaccineList);
-        fetchFullYImmunization(dateOfBirth);
-        fetchGrowthNutrition(childClient);
-        fetchBirthCertificateData(childClient);
-        fetchIllnessData(childClient);
-    }
 }
