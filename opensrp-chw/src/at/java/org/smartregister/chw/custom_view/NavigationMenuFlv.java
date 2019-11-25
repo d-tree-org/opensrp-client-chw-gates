ec74b328b821d2858288aa78ec72e0c0aa011623
Isaiah <issyzac.iz@gmail.com>
2019-11-25 13:25:23 +0300

    fix: language switcher options

--- opensrp-chw/src/at/java/org/smartregister/chw/custom_view/NavigationMenuFlv.java
+++ opensrp-chw/src/at/java/org/smartregister/chw/custom_view/NavigationMenuFlv.java
@@ -6,6 +6,6 @@ public class NavigationMenuFlv implements NavigationMenu.Flavour {
 
     @Override
     public String[] getSupportedLanguages() {
-        return new String[] { "English, Kiswahili" };
+        return new String[] { "English", "Kiswahili" };
     }
 }
