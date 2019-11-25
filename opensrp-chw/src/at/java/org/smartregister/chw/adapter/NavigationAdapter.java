90410e843dcd944bedaa62bc5462080fc954d937
Isaiah <issyzac.iz@gmail.com>
2019-11-25 15:33:54 +0300

    feat: subclassed NavigationAdapter for customizations

---
+++ opensrp-chw/src/at/java/org/smartregister/chw/adapter/NavigationAdapter.java
@@ -0,0 +1,19 @@
+package org.smartregister.chw.adapter;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+
+import org.smartregister.chw.core.model.NavigationOption;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author : Isaya Mollel on 2019-11-25.
+ */
+public class NavigationAdapter extends org.smartregister.chw.core.adapter.NavigationAdapter {
+
+    public NavigationAdapter(List<NavigationOption> navigationOptions, Activity context, Map<String, Class> registeredActivities){
+        super(navigationOptions, context, registeredActivities);
+    }
+}
