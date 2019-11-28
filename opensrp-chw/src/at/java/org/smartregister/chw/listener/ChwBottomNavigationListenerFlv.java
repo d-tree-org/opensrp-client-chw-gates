f268f72f73ec2d3e3e3b275699c1c200b9d5e47d
Isaiah <issyzac.iz@gmail.com>
2019-11-28 10:50:51 +0300

    feat: added a variant for bottom navigation listener

---
+++ opensrp-chw/src/at/java/org/smartregister/chw/listener/ChwBottomNavigationListenerFlv.java
@@ -0,0 +1,54 @@
+package org.smartregister.chw.listener;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.smartregister.chw.R;
+import org.smartregister.chw.activity.FamilyRegisterActivity;
+import org.smartregister.chw.activity.JobAidsActivity;
+import org.smartregister.view.activity.BaseRegisterActivity;
+
+/**
+ * Author : Isaya Mollel on 2019-11-28.
+ */
+public class ChwBottomNavigationListenerFlv extends DefaultChwBottomNavigationListenerFlv {
+
+    @Override
+    public boolean navigationItemSelected(MenuItem item, Activity context) {
+        BaseRegisterActivity baseRegisterActivity = (BaseRegisterActivity) context;
+
+        if (item.getItemId() == org.smartregister.family.R.id.action_family) {
+            if (context instanceof FamilyRegisterActivity) {
+                baseRegisterActivity.switchToBaseFragment();
+            } else {
+                Intent intent = new Intent(context, FamilyRegisterActivity.class);
+                context.startActivity(intent);
+                context.finish();
+            }
+        }
+        else if (item.getItemId() == R.id.action_scan_fingerprint) {
+            Toast.makeText(context, "Scanning Fingerprints", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if (item.getItemId() == org.smartregister.family.R.id.action_register) {
+
+            if (context instanceof FamilyRegisterActivity) {
+                baseRegisterActivity.startRegistration();
+            } else {
+                FamilyRegisterActivity.startFamilyRegisterForm(context);
+            }
+
+            return false;
+        }
+        else if (item.getItemId() == org.smartregister.family.R.id.action_job_aids) {
+            //view.setSelectedItemId(R.id.action_family);
+            Intent intent = new Intent(context, JobAidsActivity.class);
+            context.startActivity(intent);
+            return false;
+        }
+
+        return true;
+    }
+}
