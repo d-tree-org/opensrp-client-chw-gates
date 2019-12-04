67ddbcce2d44a7257f7f2534f1c893dbeeb07c68
Rodgers Andati <andatirodgers@gmail.com>
2019-12-04 10:10:01 +0300

    add child danger signs
    fixes #1

--- opensrp-chw/src/at/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
+++ opensrp-chw/src/at/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
@@ -1,5 +1,184 @@
 package org.smartregister.chw.interactor;
 
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.actionhelper.DangerSignsAction;
+import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.Utils;
+import org.smartregister.chw.util.Constants;
+import org.smartregister.chw.util.JsonFormUtils;
+import org.smartregister.immunization.domain.ServiceWrapper;
+
+import java.text.MessageFormat;
+import java.util.Map;
+
+import timber.log.Timber;
+import org.smartregister.chw.R;
+
 public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractorFlv {
+
+    @Override
+    protected void bindEvents(Map<String, ServiceWrapper> serviceWrapperMap) throws BaseAncHomeVisitAction.ValidationException {
+        try {
+            evaluateDangerSigns();
+            evaluateImmunization();
+
+            evaluateExclusiveBreastFeeding(serviceWrapperMap);
+            evaluateVitaminA(serviceWrapperMap);
+            evaluateDeworming(serviceWrapperMap);
+            evaluateCounselling();
+            evaluateNutritionStatus();
+            evaluateObsAndIllness();
+        } catch (BaseAncHomeVisitAction.ValidationException e) {
+            throw (e);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    private void evaluateDangerSigns() throws Exception {
+        BaseAncHomeVisitAction danger_signs = new BaseAncHomeVisitAction.Builder(context, context.getString(R.string.anc_home_visit_danger_signs))
+                .withOptional(false)
+                .withDetails(details)
+                .withFormName(Utils.getLocalForm("child_danger_signs", CoreConstants.JSON_FORM.locale, CoreConstants.JSON_FORM.assetManager))
+                .withHelper(new DangerSignsAction())
+                .build();
+        actionList.put(context.getString(R.string.anc_home_visit_danger_signs), danger_signs);
+    }
+
+    private void evaluateCounselling() throws Exception {
+        HomeVisitActionHelper counsellingHelper = new HomeVisitActionHelper() {
+            private String couselling_child;
+
+            @Override
+            public void onPayloadReceived(String jsonPayload) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonPayload);
+                    couselling_child = org.smartregister.chw.util.JsonFormUtils.getCheckBoxValue(jsonObject, "pnc_counselling");
+                } catch (JSONException e) {
+                    Timber.e(e);
+                }
+            }
+
+            @Override
+            public String evaluateSubTitle() {
+                return MessageFormat.format("{0}: {1}", "Counselling", couselling_child);
+            }
+
+            @Override
+            public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
+                if (StringUtils.isNotBlank(couselling_child)) {
+                    return BaseAncHomeVisitAction.Status.COMPLETED;
+                } else {
+                    return BaseAncHomeVisitAction.Status.PENDING;
+                }
+            }
+        };
+
+        BaseAncHomeVisitAction action = new BaseAncHomeVisitAction.Builder(context, context.getString(R.string.pnc_counselling))
+                .withOptional(false)
+                .withDetails(details)
+                .withFormName(Constants.JSON_FORM.PNC_HOME_VISIT.getCOUNSELLING())
+                .withHelper(counsellingHelper)
+                .build();
+        actionList.put(context.getString(R.string.pnc_counselling), action);
+    }
+
+    @Override
+    protected int immunizationCeiling() {
+        return 60;
+    }
+
+    private void evaluateNutritionStatus() throws BaseAncHomeVisitAction.ValidationException {
+        HomeVisitActionHelper nutritionStatusHelper = new HomeVisitActionHelper() {
+            private String nutritionStatus;
+
+            @Override
+            public void onPayloadReceived(String jsonPayload) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonPayload);
+                    nutritionStatus = JsonFormUtils.getValue(jsonObject, "nutrition_status_1m5yr").toLowerCase();
+                } catch (JSONException e) {
+                    Timber.e(e);
+                }
+            }
+
+            @Override
+            public String evaluateSubTitle() {
+                return MessageFormat.format("{0}: {1}", context.getString(R.string.nutrition_status), nutritionStatus);
+            }
+
+            @Override
+            public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
+                if (StringUtils.isBlank(nutritionStatus))
+                    return BaseAncHomeVisitAction.Status.PENDING;
+
+                return BaseAncHomeVisitAction.Status.COMPLETED;
+            }
+        };
+
+        BaseAncHomeVisitAction observation = new BaseAncHomeVisitAction.Builder(context, context.getString(R.string.anc_home_visit_nutrition_status))
+                .withOptional(false)
+                .withDetails(details)
+                .withFormName(Constants.JSON_FORM.CHILD_HOME_VISIT.getNutritionStatus())
+                .withHelper(nutritionStatusHelper)
+                .build();
+        actionList.put(context.getString(R.string.anc_home_visit_nutrition_status), observation);
+    }
+
+    @Override
+    protected void evaluateObsAndIllness() throws BaseAncHomeVisitAction.ValidationException {
+        class ObsIllnessBabyHelper extends HomeVisitActionHelper {
+            private String date_of_illness;
+            private String illness_description;
+            private String action_taken;
+            private LocalDate illnessDate;
+
+            @Override
+            public void onPayloadReceived(String jsonPayload) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonPayload);
+                    date_of_illness = JsonFormUtils.getValue(jsonObject, "date_of_illness");
+                    illness_description = JsonFormUtils.getValue(jsonObject, "illness_description");
+                    action_taken = JsonFormUtils.getValue(jsonObject, "action_taken_1m5yr");
+                    illnessDate = DateTimeFormat.forPattern("dd-MM-yyyy").parseLocalDate(date_of_illness);
+                } catch (Exception e) {
+                    Timber.e(e);
+                }
+            }
+
+            @Override
+            public String evaluateSubTitle() {
+                if (illnessDate == null)
+                    return "";
+
+                return MessageFormat.format("{0}: {1}\n {2}: {3}",
+                        DateTimeFormat.forPattern("dd MMM yyyy").print(illnessDate),
+                        illness_description, context.getString(R.string.action_taken), action_taken
+                );
+            }
+
+            @Override
+            public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
+                if (StringUtils.isBlank(date_of_illness))
+                    return BaseAncHomeVisitAction.Status.PENDING;
+
+                return BaseAncHomeVisitAction.Status.COMPLETED;
+            }
+        }
+
+        BaseAncHomeVisitAction observation = new BaseAncHomeVisitAction.Builder(context, context.getString(R.string.anc_home_visit_observations_n_illnes))
+                .withOptional(true)
+                .withDetails(details)
+                .withFormName(Constants.JSON_FORM.getObsIllness())
+                .withHelper(new ObsIllnessBabyHelper())
+                .build();
+        actionList.put(context.getString(R.string.anc_home_visit_observations_n_illnes), observation);
+    }
 
 }
