b57225e567a337a93004928a18eecc917f7bdf9c
gosso22 <sheghembekassim@gmail.com>
2020-02-06 14:41:01 +0300

    KS | Chore, added Sync Packages and include Afyatek SettingsSyncIntentService class.

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/job/ChwJobCreator.java
@@ -0,0 +1,34 @@
+package org.smartregister.chw.job;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.evernote.android.job.Job;
+import com.evernote.android.job.JobCreator;
+
+public class ChwJobCreator implements JobCreator {
+
+    private ChwJobCreator.Flavor flavor;
+
+    public ChwJobCreator() {
+        this.flavor = new ChwJobCreatorFlv();
+    }
+
+    public Flavor getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(Flavor flavor) {
+        this.flavor = flavor;
+    }
+
+    @Nullable
+    @Override
+    public Job create(@NonNull String tag) {
+        return this.flavor.getJob(tag);
+    }
+
+    public interface Flavor {
+        Job getJob(String tag);
+    }
+}
