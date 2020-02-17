ff4548ffb8158ff7e68680d77a8623f57d2183a4
issyzac <issyzac.iz@gmail.com>
2020-02-17 14:45:58 +0300

    Included child referral from danger sign screening

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
@@ -1,20 +1,111 @@
 package org.smartregister.chw.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.domain.Form;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
 import org.smartregister.chw.anc.presenter.BaseAncHomeVisitPresenter;
+import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.core.activity.CoreChildHomeVisitActivity;
 import org.smartregister.chw.core.interactor.CoreChildHomeVisitInteractor;
+import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.interactor.ChildHomeVisitInteractorFlv;
+import org.smartregister.family.util.Constants;
+import org.smartregister.family.util.JsonFormUtils;
+
+import timber.log.Timber;
 
 public class ChildHomeVisitActivity extends CoreChildHomeVisitActivity {
+
     @Override
     protected void registerPresenter() {
         presenter = new BaseAncHomeVisitPresenter(memberObject, this, new CoreChildHomeVisitInteractor(new ChildHomeVisitInteractorFlv()));
     }
 
     @Override
+    public void startFormActivity(JSONObject jsonForm) {
+
+        Form form = new Form();
+        form.setActionBarBackground(org.smartregister.chw.core.R.color.family_actionbar);
+        form.setWizard(false);
+
+        Intent intent = new Intent(this, ReferralWizardFormActivity.class);
+        intent.putExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+        intent.putExtra(Constants.WizardFormActivity.EnableOnCloseDialog, false);
+        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+        startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+
+    }
+
+    @Override
     public void submittedAndClose() {
         super.submittedAndClose();
         ChildProfileActivity.startMe(this, false, memberObject, ChildProfileActivity.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == org.smartregister.chw.anc.util.Constants.REQUEST_CODE_GET_JSON) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    String jsonString = data.getStringExtra(org.smartregister.chw.anc.util.Constants.JSON_FORM_EXTRA.JSON);
+
+                    //check if client is being referred
+                    JSONObject form = new JSONObject(jsonString);
+                    JSONArray a = form.getJSONObject("step1").getJSONArray("fields");
+                    String buttonAction = "";
+
+                    for (int i=0; i<a.length(); i++) {
+                        org.json.JSONObject jo = a.getJSONObject(i);
+                        if (jo.getString("key").compareToIgnoreCase("save_n_link") == 0 || jo.getString("key").compareToIgnoreCase("save_n_refer") == 0) {
+                            if(jo.optString("value") != null && jo.optString("value").compareToIgnoreCase("true") == 0){
+                                buttonAction = jo.getJSONObject("action").getString("behaviour");
+                            }
+                        }
+                    }
+                    if(!buttonAction.isEmpty()) {
+                        //refer
+                        if (baseEntityID!=null){
+                            CoreReferralUtils.createReferralEvent(ChwApplication.getInstance().getContext().allSharedPreferences(),
+                                    jsonString, "ec_anc_referral", baseEntityID);
+                        }else {
+                            CoreReferralUtils.createReferralEvent(ChwApplication.getInstance().getContext().allSharedPreferences(),
+                                    jsonString, "ec_anc_referral", memberObject.getBaseEntityId());
+                        }
+
+                        this.finish();
+                    }
+
+                    //end of check referral
+                    BaseAncHomeVisitAction ancHomeVisitAction = actionList.get(current_action);
+                    if (ancHomeVisitAction != null) {
+                        ancHomeVisitAction.setJsonPayload(jsonString);
+                    }
+                } catch (Exception e) {
+                    Timber.e(e);
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+
+                BaseAncHomeVisitAction ancHomeVisitAction = actionList.get(current_action);
+                if (ancHomeVisitAction != null)
+                    ancHomeVisitAction.evaluateStatus();
+            }
+
+        }
+
+        // update the adapter after every payload
+        if (mAdapter != null) {
+            mAdapter.notifyDataSetChanged();
+            redrawVisitUI();
+        }
     }
 
 }
