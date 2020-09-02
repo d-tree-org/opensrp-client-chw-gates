76c19521c020d4cd2250e35269df01e751a1d3bf
issyzac <issyzac.iz@gmail.com>
2020-09-02 15:52:16 +0300

    added method contracts to update pregnancy outcome

--- opensrp-chw/src/main/java/org/smartregister/chw/contract/PncMemberProfileContract.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/PncMemberProfileContract.java
@@ -2,6 +2,7 @@ package org.smartregister.chw.contract;
 
 import org.json.JSONObject;
 import org.smartregister.chw.anc.contract.BaseAncMemberProfileContract;
+import org.smartregister.chw.anc.contract.BaseAncRegisterContract;
 import org.smartregister.chw.model.ReferralTypeModel;
 import org.smartregister.chw.pnc.contract.BasePncMemberProfileContract;
 import org.smartregister.repository.AllSharedPreferences;
@@ -14,6 +15,9 @@ public interface PncMemberProfileContract {
         void startFormActivity(JSONObject formJson);
 
         List<ReferralTypeModel> getReferralTypeModels();
+
+        void onPregnancyOutcomeUpdated(boolean successful);
+
     }
 
     interface Presenter extends BasePncMemberProfileContract.Presenter {
@@ -21,11 +25,20 @@ public interface PncMemberProfileContract {
 
         void referToFacility();
 
+        void saveForm(String json, String table);
+
         void createReferralEvent(AllSharedPreferences allSharedPreferences, String jsonString) throws Exception;
     }
 
     interface Interactor extends BasePncMemberProfileContract.Interactor, BaseAncMemberProfileContract.Interactor {
         void createReferralEvent(AllSharedPreferences allSharedPreferences, String jsonString, String entityID) throws Exception;
+
+        void updatePregnancyOutcome(final String jsonString, final PncMemberProfileContract.InteractorCallBack callBack, final String table);
+
+    }
+
+    interface InteractorCallBack {
+        void onSaved(boolean result);
     }
 
 }
