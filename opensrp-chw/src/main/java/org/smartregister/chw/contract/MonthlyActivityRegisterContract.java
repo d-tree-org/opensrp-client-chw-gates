4d3252472bb8fc7ccdfa9d056ee67ad070dfd75e
issyzac <issyzac.iz@gmail.com>
2020-10-26 15:20:56 +0300

    Registered a contract for monthly activity register

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/MonthlyActivityRegisterContract.java
@@ -0,0 +1,15 @@
+package org.smartregister.chw.contract;
+
+public interface MonthlyActivityRegisterContract{
+
+    public interface Interactor {}
+
+    public interface Model {
+
+    }
+
+    public interface Presenter extends  org.smartregister.view.contract.BaseRegisterContract.Presenter {
+
+    }
+
+}
