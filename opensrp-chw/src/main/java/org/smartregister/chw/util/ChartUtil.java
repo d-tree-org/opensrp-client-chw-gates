d3e296cc7b6bac9051c3ba973b4a7e87a00ff924
issyzac <issyzac.iz@gmail.com>
2020-11-03 12:48:46 +0300

    added a helper class for monthly activities dashboard

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/ChartUtil.java
@@ -0,0 +1,20 @@
+package org.smartregister.chw.util;
+
+import android.graphics.Color;
+
+/**
+ * The ChartUtil provides util functions to work with Charts
+ *
+ * @author allan
+ */
+public class ChartUtil {
+
+    // Sample indicator keys
+    public static String numericIndicatorKey = "S_IND_001";
+    public static String pieChartYesIndicatorKey = "S_IND_002";
+    public static String pieChartNoIndicatorKey = "S_IND_003";
+
+    // Color definitions for the chart slices. This could essentially be defined in colors.xml
+    public static final int YES_GREEN_SLICE_COLOR = Color.parseColor("#99CC00");
+    public static final int NO_RED_SLICE_COLOR = Color.parseColor("#FF4444");
+}
