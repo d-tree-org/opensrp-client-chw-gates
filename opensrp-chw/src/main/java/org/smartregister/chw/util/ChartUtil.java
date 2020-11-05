831be84cc184c3b999fc0ad61c158ce9b49c22c7
issyzac <issyzac.iz@gmail.com>
2020-11-05 23:17:44 +0300

    code refactor

--- opensrp-chw/src/main/java/org/smartregister/chw/util/ChartUtil.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/ChartUtil.java
@@ -14,10 +14,10 @@ public class ChartUtil {
     public static String pieChartYesIndicatorKey = "S_IND_002";
     public static String pieChartNoIndicatorKey = "S_IND_001";
 
-    public static String currentMonthRegistrationsIndicatorKey = "S_IND_002";
-    public static String lastMonthRegistrationsIndicatorKey = "S_IND_001";
-    public static String currentMonthVisitsIndicatorKey = "";
-    public static String lastsMonthVisitsIndicatorKey = "";
+    public static String currentMonthRegistrationsIndicatorKey = "CURR_MONTH_REG";
+    public static String lastMonthRegistrationsIndicatorKey = "LAST_MONTH_REG";
+    public static String currentMonthVisitsIndicatorKey = "CURR_MONTH_VISITS";
+    public static String lastsMonthVisitsIndicatorKey = "LAST_MONTH_VISITS";
 
     // Color definitions for the chart slices. This could essentially be defined in colors.xml
     public static final int YES_GREEN_SLICE_COLOR = Color.parseColor("#99CC00");
