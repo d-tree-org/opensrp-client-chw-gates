2bc62f67a654792e27ecb1dbfd5e053a0a015336
issyzac <issyzac.iz@gmail.com>
2020-11-10 13:54:54 +0300

    Include a title for every indicator

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/MonthlyActivityDashboard.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/MonthlyActivityDashboard.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,22 +21,16 @@ import org.smartregister.chw.util.ChartUtil;
 import org.smartregister.reporting.contract.ReportContract;
 import org.smartregister.reporting.domain.IndicatorQuery;
 import org.smartregister.reporting.domain.IndicatorTally;
-import org.smartregister.reporting.domain.PieChartSlice;
 import org.smartregister.reporting.domain.ReportIndicator;
 import org.smartregister.reporting.model.NumericDisplayModel;
 import org.smartregister.reporting.view.NumericIndicatorView;
-import org.smartregister.reporting.view.PieChartIndicatorView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.smartregister.reporting.contract.ReportContract.IndicatorView.CountType.LATEST_COUNT;
-import static org.smartregister.reporting.contract.ReportContract.IndicatorView.CountType.TOTAL_COUNT;
-import static org.smartregister.reporting.util.ReportingUtil.addPieChartSlices;
 import static org.smartregister.reporting.util.ReportingUtil.getIndicatorDisplayModel;
-import static org.smartregister.reporting.util.ReportingUtil.getPieChartDisplayModel;
-import static org.smartregister.reporting.util.ReportingUtil.getPieChartSlice;
 
 public class MonthlyActivityDashboard extends Fragment implements ReportContract.View, LoaderManager.LoaderCallbacks<List<Map<String, IndicatorTally>>>  {
 
@@ -48,6 +43,7 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
     private boolean hasLoadedSampleData = true;
 
     private View spacerView;
+    private View titleView;
 
     MonthlyActivityDashboard(){}
 
@@ -113,6 +109,7 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         spacerView = inflater.inflate(R.layout.report_spacer_view, container, false);
+        titleView = inflater.inflate(R.layout.reporting_indicator_title, container, false);
         return inflater.inflate(R.layout.monthly_activities_dashboard_fragment, container, false);
     }
 
@@ -150,19 +147,21 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
 
     private void createReportViews(ViewGroup mainLayout) {
 
-        NumericDisplayModel currentMonthRegistrations = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.currentMonthRegistrationsIndicatorKey, R.string.current_month_registrations, indicatorTallies);
+        mainLayout.addView(getTitleView(getResources().getString(R.string.current_month)));
+        NumericDisplayModel currentMonthRegistrations = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.currentMonthRegistrationsIndicatorKey, R.string.registrations_indicator, indicatorTallies);
         mainLayout.addView(new NumericIndicatorView(getContext(), currentMonthRegistrations).createView());
 
-        NumericDisplayModel lastMonthRegistrations = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.lastMonthRegistrationsIndicatorKey, R.string.last_month_registrations, indicatorTallies);
-        mainLayout.addView(new NumericIndicatorView(getContext(), lastMonthRegistrations).createView());
+        NumericDisplayModel currentMonthVisits = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.currentMonthVisitsIndicatorKey, R.string.visits_indicator, indicatorTallies);
+        mainLayout.addView(new NumericIndicatorView(getContext(), currentMonthVisits).createView());
 
         //Add Space between indicators
         mainLayout.addView(spacerView);
 
-        NumericDisplayModel currentMonthVisits = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.currentMonthVisitsIndicatorKey, R.string.current_month_visits_tallies, indicatorTallies);
-        mainLayout.addView(new NumericIndicatorView(getContext(), currentMonthVisits).createView());
+        mainLayout.addView(getTitleView(getResources().getString(R.string.last_month)));
+        NumericDisplayModel lastMonthRegistrations = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.lastMonthRegistrationsIndicatorKey, R.string.registrations_indicator, indicatorTallies);
+        mainLayout.addView(new NumericIndicatorView(getContext(), lastMonthRegistrations).createView());
 
-        NumericDisplayModel lastMonthVisits = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.lastsMonthVisitsIndicatorKey, R.string.last_month_visits, indicatorTallies);
+        NumericDisplayModel lastMonthVisits = getIndicatorDisplayModel(LATEST_COUNT, ChartUtil.lastsMonthVisitsIndicatorKey, R.string.visits_indicator, indicatorTallies);
         mainLayout.addView(new NumericIndicatorView(getContext(), lastMonthVisits).createView());
 
         //PieChartSlice indicator2_1 = getPieChartSlice(LATEST_COUNT, ChartUtil.pieChartYesIndicatorKey, getResources().getString(R.string.target_not_reached), getResources().getColor(android.R.color.holo_red_light), indicatorTallies);
@@ -170,6 +169,13 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
 
         //mainLayout.addView(new PieChartIndicatorView(getContext(), getPieChartDisplayModel(addPieChartSlices(indicator2_1, indicator2_2), R.string.monthly_target, R.string.visits_and_registrations)).createView());
 
+    }
+
+    View getTitleView(String titleText){
+        View tv = LayoutInflater.from(this.getContext()).inflate(R.layout.reporting_indicator_title,null, false);
+        TextView title = tv.findViewById(R.id.indicator_title);
+        title.setText(titleText);
+        return tv;
     }
 
     @NonNull
