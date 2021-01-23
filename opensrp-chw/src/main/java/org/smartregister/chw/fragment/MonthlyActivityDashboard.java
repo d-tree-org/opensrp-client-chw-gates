2d0373c7bccbe081e8449a9be492335af869f94e
issyzac <issyzac.iz@gmail.com>
2021-01-23 13:07:05 +0300

    Updated Updated dynamic indicator query

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/MonthlyActivityDashboard.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/MonthlyActivityDashboard.java
@@ -81,7 +81,7 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
                 "from visits " +
                 "where " +
                 "datetime(visit_date/1000, 'unixepoch') > date('now', 'start of month') " +
-                "and visit_type in ('ANC Home Visit', 'PNC Home Visit') " +
+                "and visit_type in ('ANC Home Visit', 'PNC Home Visit', 'Referral Follow-up Visit', 'Linkage Follow-up Visit', 'Child Home Visit', 'Adolescent Home Visit') " +
                 "group by base_entity_id, date_visited )";
 
         String lastMonthVisitsQuery = "select count (*) " +
@@ -91,7 +91,7 @@ public class MonthlyActivityDashboard extends Fragment implements ReportContract
                 "where " +
                 "datetime(visit_date/1000, 'unixepoch') < date('now', 'start of month') " +
                 "and datetime(visit_date/1000, 'unixepoch') > date('now', 'start of month', '-1 months') " +
-                "and visit_type in ('ANC Home Visit', 'PNC Home Visit') " +
+                "and visit_type in ('ANC Home Visit', 'PNC Home Visit', 'Referral Follow-up Visit', 'Linkage Follow-up Visit', 'Child Home Visit', 'Adolescent Home Visit') " +
                 "group by base_entity_id, date_visited )";
 
         ReportIndicator currentMonthVisitsIndicator = new ReportIndicator();
