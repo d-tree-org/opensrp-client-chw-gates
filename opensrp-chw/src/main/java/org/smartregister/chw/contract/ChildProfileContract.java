35182bc33737a33fed1307249d0bd1622a6bdaeb
issyzac <issyzac.iz@gmail.com>
2021-05-24 16:31:46 +0300

    Added contract definition for fingerprint verification permission

--- opensrp-chw/src/main/java/org/smartregister/chw/contract/ChildProfileContract.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/ChildProfileContract.java
@@ -7,21 +7,26 @@ public interface ChildProfileContract extends org.smartregister.chw.core.contrac
 
     public interface Presenter {
         void verifyChildFingerprint();
+        void getChildVerifyFingerprintPermission(String childBaseEntityId);
     }
 
     public interface Interactor {
         void getChildFingerprintForVerification(String baseEntityId, InteractorCallback callback);
+        void verifyChildFingerprintPermission(String baseEntityId, InteractorCallback callback);
     }
 
     public interface InteractorCallback {
         void callFingerprintVerification(String clientFingerprint);
         void callFingerprintEnrollment(Client client);
+        void setVerifyFingerprintPermission(boolean permission);
     }
 
     public interface View {
         void callFingerprintRegister(Client client);
 
         void callFingerprintVerification(String fingerprintId);
+
+        void updateChildFingerprintVerificationPermission(boolean permission);
     }
 
 }
