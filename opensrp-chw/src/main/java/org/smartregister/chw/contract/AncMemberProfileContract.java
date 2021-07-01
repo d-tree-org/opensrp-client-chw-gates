88533fc9cfff38847d0ba8fc214a946adebedeff
issyzac <issyzac.iz@gmail.com>
2021-07-01 12:30:16 +0300

    Added interactor callback contracts

--- opensrp-chw/src/main/java/org/smartregister/chw/contract/AncMemberProfileContract.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/AncMemberProfileContract.java
@@ -1,15 +1,32 @@
 package org.smartregister.chw.contract;
 
 import org.smartregister.chw.model.ReferralTypeModel;
+import org.smartregister.domain.db.Client;
 
 import java.util.List;
 
 public interface AncMemberProfileContract extends org.smartregister.chw.core.contract.AncMemberProfileContract {
+
+    interface Interactor  {
+        void getFingerprintForVerification(String baseEntityId, InteractorCallback callBack);
+    }
+
+    interface InteractorCallback {
+        void onFingerprintFetched(String fingerprint, boolean hasFingerprint, Client client);
+    }
+
     interface Presenter{
         void referToFacility();
+
+        void verifyFingerprint();
     }
 
     interface View{
         List<ReferralTypeModel> getReferralTypeModels();
+
+        void callFingerprintVerification(String fingerprintId);
+
+        void callFingerprintRegistration(Client client);
+
     }
 }
